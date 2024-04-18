ae1faea258530809c16f26a0e5d4df1d6e0259d5
jonah-js <164077834+jonah-js@users.noreply.github.com>
2024-04-18 13:39:26 +0200

    Add files via upload

---
+++ TextDurchsucher/main.js
@@ -0,0 +1,26 @@
+let btn = document.getElementById('btn');
+function scanner(){
+    let input11 = document.getElementById('input1').value;
+    let input22 = document.getElementById('input2').value;
+    let finish = input11.includes(input22);
+    console.log(finish);
+}
+
+/*
+let hel = prompt("Welchen Text");
+let input = prompt("wonach suchen?");
+let hel1 = hel.includes(input);
+console.log(hel1);*/
+
+
+
+
+
+
+
+/*<script>
+    $("#mybtn").click(function(){
+        console.log('clicked')
+    });
+</script>*/
+
